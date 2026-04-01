49a88396e779d30d9180831c166bcfb3e8a8fd7a
Hexterty46 <efsuslusemih@gmail.com>
2026-04-02 00:53:47 +0200

    Dit is voor om te checken wanneer, of de pagina word geladen met javascript of niet. Zo niet dan komt er een button om de menu et kunnen sluiten zonder javascript
    #17

--- public/script.js
+++ public/script.js
@@ -1,14 +1,16 @@
 const menuOpen = document.querySelector("#menuOpen")
-// const menuClose = document.querySelector('#menuSluiten')
 const menu = document.querySelector('.menu')
 
-console.log('test')
 
-menuOpen.addEventListener('click', function(event) {
-    menu.classList.toggle('is-open')
-    // document.body.style.overflow = 'hidden'
-    event.preventDefault()
-})
+document.documentElement.classList.remove("no-js");
+document.documentElement.classList.add("js");
+
+if (menuOpen && menu) {
+  menuOpen.addEventListener("click", function (event) {
+    menu.classList.toggle("is-open");
+    event.preventDefault();
+  });
+}
 
 // menuClose.addEventListener('click', () => {
 //     menu.classList.remove('is-open')
