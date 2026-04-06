3eaff22d13053d4675e6f5f73f1b54c70a4008c7
Hexterty46 <efsuslusemih@gmail.com>
2026-04-07 01:12:56 +0200

    Verzenden knop zelfde hoogte gemaakt. Er kan niet meer gescrolled worden als de menu geopend is.
    #21

--- public/script.js
+++ public/script.js
@@ -1,13 +1,19 @@
 const menuOpen = document.querySelector("#menuOpen")
 const menu = document.querySelector('.menu')
+const root = document.documentElement
 
-
-document.documentElement.classList.remove("no-js");
-document.documentElement.classList.add("js");
+document.documentElement.classList.remove("no-js")
+document.documentElement.classList.add("js")
 
 if (menuOpen && menu) {
   menuOpen.addEventListener("click", function (event) {
-    menu.classList.toggle("is-open");
-    event.preventDefault();
-  });
+    event.preventDefault()
+    const isOpen = menu.classList.toggle("is-open")
+
+    if (isOpen) {
+      root.classList.add('menu-open')
+    } else {
+      root.classList.remove('menu-open')
+    }
+  })
 }
